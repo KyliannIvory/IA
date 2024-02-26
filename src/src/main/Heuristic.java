1ad4936f0001ab7f0d1e881be9b674b76625e3d4
kyliann <kouassi-jean-marie-k.KOUAME@etu.univ-amu.fr>
2024-02-26 15:30:58 +0100

    test

--- src/src/main/Heuristic.java
+++ src/src/main/Heuristic.java
@@ -2,7 +2,7 @@ package main;
 
 public interface Heuristic {
 
-    void calculateHeuristic(State currentState);
+    void calculateHeuristic(State state);
     // mais je pense qu'on va devoir avoir un constructeur qui va prendre l'etat finale pour effectuer les calcules
 
 }
